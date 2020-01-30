81ae8a4443ac4255acc8cb7a783f813c7a79f6be
Stephen Rubio <stephenrubio@protonmail.com>
2020-01-29 20:55:21 -0800

    Updated to 2.24, added C++

---
+++ Source/Derelict54VR/Derelict54VR.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Derelict54VR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Derelict54VR, "Derelict54VR" );
